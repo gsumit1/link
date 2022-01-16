1b4f310a4c5cf864e1b6ccd25b1f4694a80c2dec
Sumit Ghosh <ghosh.sumit1@gmail.com>
2022-01-16 14:53:37 +0530

    updated

--- src/com/test/demo/Test.java
+++ src/com/test/demo/Test.java
@@ -1,8 +1,13 @@
 package com.test.demo;
 
 public class Test {
-	int a=10;
-	int b=20;
-	int c=a*b;
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a * b;
+		System.out.println(c);
+
+	}
 
 }
